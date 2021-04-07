ac39e065f80e9e5a441a3aafc5e40671f8be628d
Valar <admin@valar-solutions.com>
2021-04-07 20:01:03 +0700

    feat: add IUniswapV2Pair

---
+++ contracts/interfaces/IUniswapV2Pair.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+interface IUniswapV2Pair {
+    
+    function sync()
+        external;
+        
+    function getReserves()
+        external
+        view
+        returns (uint, uint, uint32);
+        
+    function token0()
+        external
+        view
+        returns (address);
+        
+    function token1()
+        external
+        view
+        returns (address);
+}
