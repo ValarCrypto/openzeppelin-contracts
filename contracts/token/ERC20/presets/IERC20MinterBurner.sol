78dd2a2574c7f027fb6e10b8de1f1812b86876ea
Valar <admin@valar-solutions.com>
2021-04-07 21:56:23 +0700

    chore: interface update

--- contracts/token/ERC20/presets/IERC20MinterBurner.sol
+++ contracts/token/ERC20/presets/IERC20MinterBurner.sol
@@ -2,9 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-import "../ERC20.sol";
-import "../../../access/AccessControlEnumerable.sol";
-import "../../../utils/Context.sol";
+import "../IERC20.sol";
 
 /**
  * @dev {ERC20} token, including:
