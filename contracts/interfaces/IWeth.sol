6950ec20728200997551a75d1dada7f23e7990d4
Valar <admin@valar-solutions.com>
2021-04-07 20:00:00 +0700

    feat: add IWeth

---
+++ contracts/interfaces/IWeth.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.3;
+
+interface IWeth {
+
+    function balanceOf(
+        address account
+    )
+        external
+        view
+        returns (uint);
+        
+
+    function transfer(
+        address _to,
+        uint _value
+    )  external returns (
+        bool success
+    );
+        
+        
+    function deposit()
+        external
+        payable;
+
+    function withdraw(
+        uint wad
+    ) external;
+
+    function approve(
+        address _spender,
+        uint _value
+    )  external returns (
+        bool success
+    );
+}
