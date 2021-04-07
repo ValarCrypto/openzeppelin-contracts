9d003fe144312adf5bcf3674ab3319574cc2ab92
Valar <admin@valar-solutions.com>
2021-04-07 21:25:11 +0700

    feat: add IERC20MinerBurner

---
+++ contracts/token/ERC20/presets/IERC20MinerBurner.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "../ERC20.sol";
+import "../../../access/AccessControlEnumerable.sol";
+import "../../../utils/Context.sol";
+
+/**
+ * @dev {ERC20} token, including:
+ *
+ *  - ability for holders to burn (destroy) their tokens
+ *  - a minter role that allows for token minting (creation)
+ *  - a pauser role that allows to stop all token transfers
+ *
+ * This contract uses {AccessControl} to lock permissioned functions using the
+ * different roles - head to its documentation for details.
+ *
+ * The account that deploys the contract will be granted the minter and pauser
+ * roles, as well as the default admin role, which will let it grant both minter
+ * and pauser roles to other accounts.
+ */
+interface ERC20MinterBurner is Context, AccessControlEnumerable, ERC20 {
+
+    /**
+     * @dev Creates `amount` new tokens for `to`.
+     *
+     * See {ERC20-_mint}.
+     *
+     * Requirements:
+     *
+     * - the caller must have the `MINTER_ROLE`.
+     */
+    function mint(address to, uint256 amount) public virtual;
+
+    /**
+     * @dev Burns `amount` new tokens for `to`.
+     *
+     * See {ERC20-_burn}.
+     *
+     * Requirements:
+     *
+     * - the caller must have the `BURNER_ROLE`.
+     */
+    function burn(address to, uint256 amount) public virtual;
+}
