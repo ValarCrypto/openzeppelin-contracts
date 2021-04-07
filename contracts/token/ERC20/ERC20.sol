b9fded2b603764b6a0a31a70c60925d837b4b965
Valar <admin@valar-solutions.com>
2021-04-07 13:14:11 +0700

    chore: remove memory

--- contracts/token/ERC20/ERC20.sol
+++ contracts/token/ERC20/ERC20.sol
@@ -50,7 +50,7 @@ contract ERC20 is Context, IERC20, IERC20Metadata {
      * All two of these values are immutable: they can only be set once during
      * construction.
      */
-    constructor (string memory name_, string memory symbol_, uint memory decimals_) {
+    constructor (string memory name_, string memory symbol_, uint decimals_) {
         _name = name_;
         _symbol = symbol_;
         _decimals = decimals_;
