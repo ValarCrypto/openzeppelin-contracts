8d6800f1c78556629b376900abf1fa360173207f
Valar <admin@valar-solutions.com>
2021-04-08 22:53:40 +0700

    feat: add new function

--- contracts/interfaces/IUniswapV2Router.sol
+++ contracts/interfaces/IUniswapV2Router.sol
@@ -41,4 +41,12 @@ interface IUniswapV2Router {
         address to,
         uint deadline
     ) external returns (uint amountToken, uint amountETH);
+    
+    function swapExactTokensForTokens(
+      uint amountIn,
+      uint amountOutMin,
+      address[] calldata path,
+      address to,
+      uint deadline
+    ) external returns (uint[] memory amounts);
 }
