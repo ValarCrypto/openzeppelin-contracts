8795f4c1b999a18e51284a3e586fe4d630c50202
Valar <admin@valar-solutions.com>
2021-04-09 11:29:38 +0700

    feat: add interface

---
+++ contracts/interfaces/IValancerUtils.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+interface IValancerUtils {
+    
+    
+    function getPriceMultiplierOverTime()
+        external
+        view
+        returns (uint);
+    
+    
+    function getLiquidity(address pairContract, address tokenL, address tokenR)
+        external
+        view
+        returns (uint, uint, address, address);
+    
+    function getTokenPrice(address token)
+        external
+        view
+        returns (uint);
+        
+    function getValancerBuyAmount(uint balanceV, uint tokenBalanceWithPrice)
+        external
+        pure
+        returns (uint);
+}
