a98d8064dd1a518d645a50b68582b525b569056c
Valar <admin@valar-solutions.com>
2021-04-09 18:19:16 +0700

    chore: add new method

--- contracts/interfaces/IValancerUtils.sol
+++ contracts/interfaces/IValancerUtils.sol
@@ -5,6 +5,11 @@ pragma solidity ^0.8.0;
 interface IValancerUtils {
     
     
+    function checkTokenLiquidity(address token, uint minimumUsdcEquivalent)
+        public
+        view
+        returns(bool);
+        
     function getPriceMultiplierOverTime()
         external
         view
